b20552465dfc44f6b52380983fe9c674579b7899
IkumaTadokoro <tadokorodev@gmail.com>
2020-11-07 19:35:32 +0900

    Add sort by modify time

--- lib/memo.rb
+++ lib/memo.rb
@@ -7,7 +7,9 @@ class Memo
   MEMO_DIR = './memos'
 
   def self.index
-    Dir.glob("#{MEMO_DIR}/*").map { |file| JSON.parse(File.read(file), symbolize_names: true) }
+    Dir.glob("#{MEMO_DIR}/*")
+       .sort_by { |file| File.mtime(file) }
+       .map { |file| JSON.parse(File.read(file), symbolize_names: true) }
   end
 
   def self.show(id:)
