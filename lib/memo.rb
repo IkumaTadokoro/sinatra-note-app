7d737f5d83b4cfdc71d1b55ac0ec73d466df5773
IkumaTadokoro <tadokorodev@gmail.com>
2020-11-16 22:37:19 +0900

    Use instance variable in Memo#destroy

--- lib/memo.rb
+++ lib/memo.rb
@@ -39,7 +39,7 @@ class Memo
     @@connection.exec('UPDATE memo SET title = $1, content = $2 WHERE id = $3', [title, content, id])
   end
 
-  def destroy(id:)
-    @@connection.exec('DELETE FROM memo WHERE id = $1', [id])
+  def destroy
+    @@connection.exec('DELETE FROM memo WHERE id = $1', [@id])
   end
 end
